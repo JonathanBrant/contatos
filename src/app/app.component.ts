be50aee1a82eb63d74468a91d48a0f4ea8c88c55
Jonathan Brant <jbrant1995@gmail.com>
2021-09-01 16:31:19 -0300

    bug fix modal true

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contatos';
-  mostrandoModal = true;
+  mostrandoModal = false;
 
   mostrarModal() {
       this.mostrandoModal = true;
